994cd445e45d3b7b2bc43c980d3b75ea0cab3070
Angela Gola <angelagola.ko@gmail.com>
2022-05-12 12:45:44 -0400

    Trying to add API and API-key

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,17 @@
 let nowMoment = moment().format("MMMM Do YYYY");
 let displayDate = document.getElementById("currentDay");
 displayDate.innerHTML = nowMoment;
+
+  fetch("//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}")
+  .then(function(response) {
+  response.json().then(function(data) {
+    console.log(data);
+  });
+});
+
+
+  var response = fetch("");
+console.log(response);
+
+//api key: 8382807303ccd8efb6fc8344617069a3
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
