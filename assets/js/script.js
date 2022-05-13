ae4c919c0763643a2af0ee6ba9f010bd070c583d
Angela Gola <angelagola.ko@gmail.com>
2022-05-13 12:05:58 -0400

    Trying to get API to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,43 @@ let nowMoment = moment().format("MMMM Do YYYY");
 let displayDate = document.getElementById("currentDay");
 displayDate.innerHTML = nowMoment;
 
-  fetch("//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}")
-  .then(function(response) {
-  response.json().then(function(data) {
-    console.log(data);
-  });
-});
+var cityName="";
+var cities=[];
+var cityFormE1=document.querySelector("#city-search-form");
+var cityInputE1=document.querySelector("#city");
+
+var formSubmitHandler = function(event){
+    var city = cityInputE1.value.trim();
+    if (city) {
+        console.log(city);
+    } else {
+        alert("Please enter a city");
+    }
+    saveSearch();
+}
 
 
-  var response = fetch("");
-console.log(response);
+var apiKey="8382807303ccd8efb6fc8344617069a3";
 
-//api key: 8382807303ccd8efb6fc8344617069a3
-//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+function currentWeather(city){
+    var queryURL= "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + apiKey;
+    then(function(response){
+        console.log(response)
+    }
+)};
+
+currentWeather();
+
+//Then in the link to fetch the api
+//var apiLink = "linktofetch" + apiKey
+//fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=8382807303ccd8efb6fc8344617069a3")
+//.then(function(response) {
+//response.json().then(function(data) {
+//  console.log(data);
+//});
+//});
+
+//var response = fetch("");
+//console.log(response);
+
+cityFormE1.addEventListener("submit", formSubmitHandler);
