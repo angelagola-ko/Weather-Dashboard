2f39acbbfb8244b9e94714ed989e72824e4e0c33
Angela Gola <angelagola.ko@gmail.com>
2022-05-14 11:41:44 -0400

    Added temp to eachday

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,19 @@ let nowMoment = moment().format("MMMM Do YYYY");
 let displayDate = document.getElementById("currentDay");
 displayDate.innerHTML = nowMoment;
 
-var cityName="";
+var cityName="Sarasota";
 var cities=[];
 var cityFormE1=document.querySelector("#city-search-form");
 var cityInputE1=document.querySelector("#city");
-console.log(cityFormE1)
+//console.log(cityFormE1)
 var formSubmitHandler = function(event){
-    console.log("Heeeello")
+  //  console.log("Heeeello")
     var city = cityInputE1.value.trim();
     if (city) {
-        console.log(city);
+     //   console.log(city);
         currentWeather(city);
     } else {
-        alert("Please enter a city");
+    //    alert("Please enter a city");
     }
    // saveSearch();
 }
@@ -28,9 +28,9 @@ function currentWeather(city){
     fetch(queryURL).then(function(response) {
         if(response.ok) {
             response.json().then(function(data) {
-                console.log(data);
+                //     console.log(data);
                 currentCityWeather(data[0].lat, data[0].lon)
-        });
+            });
         } else {
             alert("Error: " + response.statusText);
         }
@@ -42,6 +42,9 @@ function currentCityWeather(lat,lon){
         if(response.ok) {
             response.json().then(function(data) {
                 console.log(data);
+                console.log(data.hourly[0].temp);
+                forecast(data);
+                //    console.log(data.)
             });
         } else {
             alert("Error:" +response.statusText);
@@ -49,8 +52,15 @@ function currentCityWeather(lat,lon){
     })
 }
 
-
+function forecast(data) {
+    for (i=0; i<5; i++) {
+        var li=document.getElementById("dailyWeather"+ i);
+        console.log("dailyWeather" + i);
+        var tempF=(((data.daily[i].temp.day)-275.15)*9/5 +32);
+        li.innerHTML=tempF;
+        console.log(tempF)
+    }
+}
 //currentWeather(cityName);
-
 
 cityFormE1.addEventListener("click", formSubmitHandler);
